b4adc180dae1f3e70f6001643bbfaf71fe3859f9
Andrej <blackflash@centrum.sk>
2015-11-27 22:07:19 +0100

    button fix

--- Poker2MadHack/obj/Debug/android/poker2madhack/poker2madhack/R.java
+++ Poker2MadHack/obj/Debug/android/poker2madhack/poker2madhack/R.java
@@ -12,16 +12,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int monoandroidsplash=0x7f020001;
     }
     public static final class id {
-        public static final int PlayButton=0x7f060006;
-        public static final int editText1=0x7f060001;
-        public static final int editText2=0x7f060003;
-        public static final int editText3=0x7f060005;
-        public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060002;
-        public static final int textView3=0x7f060004;
+        public static final int PlayButton=0x7f050006;
+        public static final int editText1=0x7f050001;
+        public static final int editText2=0x7f050003;
+        public static final int editText3=0x7f050005;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050002;
+        public static final int textView3=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,8 +28,5 @@ public final class R {
     public static final class string {
         public static final int ApplicationName=0x7f040001;
         public static final int Hello=0x7f040000;
-    }
-    public static final class style {
-        public static final int Mono_Android_Theme_Splash=0x7f050000;
     }
 }
