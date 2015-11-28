9eec869491c5e1525df214133c7a5fc3df447547
Andrej <blackflash@centrum.sk>
2015-11-28 12:19:48 +0100

    added circle attributes

--- Poker2/MainActivity.cs
+++ Poker2/MainActivity.cs
@@ -2,7 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Widget;
-
+using com.refractored.monodroidtoolkit;
 
 namespace Poker2
 {
@@ -10,6 +10,7 @@ namespace Poker2
     public class MainActivity : Activity
     {
         int count = 1;
+        private HoloCircularProgressBar m_Progress;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -23,6 +24,11 @@ namespace Poker2
 
             var createGameButton = FindViewById<ImageButton>(Resource.Id.myButton);
             createGameButton.Click += delegate { StartActivity(typeof(CreateGame));};
+
+            m_Progress = FindViewById<HoloCircularProgressBar>(Resource.Id.holoCircularProgressBar1);
+            m_Progress.Indeterminate = true;
+            m_Progress.Progress = 10;
+            m_Progress.ProgressColor = 000000;
 
             //var joinGamebutton = FindViewById<Button>(Resource.Id.JoinGameButton);
             //joinGamebutton.Click += delegate { StartActivity(typeof(JoinGame)); };
